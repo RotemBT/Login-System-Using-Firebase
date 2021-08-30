33b89f9d8d13d7d042b8446e841960665b860695
Rotem Ben Tulila <68068799+RotemBT@users.noreply.github.com>
2021-08-30 13:37:01 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyAe-KHVfaugniyj3GB_Etj2WI1LEx7-01Q",
-  authDomain: "auth-development-82fb9.firebaseapp.com",
-  databaseURL: "https://auth-development-82fb9-default-rtdb.firebaseio.com",
-  projectId: "auth-development-82fb9",
-  storageBucket: "auth-development-82fb9.appspot.com",
-  messagingSenderId: "747038168401",
-  appId: "1:747038168401:web:b442aadc5fe93596e8af70",
+  //Enter your API key from firebase
 });
 
 export const auth = app.auth();
